925b1462ddc59f14ec3e74c177a421aacada8e2e
corentin jezierski <corentin.jezierski@gmail.com>
2022-04-02 23:52:44 +0200

    j'essaie de regler mon probleme sur le carrousel

--- js/caroussel.js
+++ js/caroussel.js
@@ -1,6 +1,6 @@
 var index_slide = 0
 
-function desactiveSlide(){
+async function desactiveSlide(){
     var items = document.querySelectorAll(".pictureProject");
     while (items[index_slide].classList.contains("active")){
         items[index_slide].classList.remove('active');
@@ -15,7 +15,7 @@ function activeSlide(){
 function changeSlide(n){
     var items = document.querySelectorAll(".pictureProject");
     var nbItems = items.length;
-    desactiveSlide(index_slide);
+    var response = await desactiveSlide(index_slide);
     index_slide= (index_slide+n+nbItems)%nbItems;
     activeSlide(index_slide);
 }
